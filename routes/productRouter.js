892c5fc7d394468a435595f50a0b461f83875c17
ansumandas441 <ansuman441@gmail.com>
2024-01-03 21:31:12 +0530

    Added the product services and tested the addProduct

---
+++ routes/productRouter.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+const productController = require('../controllers/productController');
+
+// Route for adding a product
+router.post('/add', productController.addProduct);
+
+//Route for editing a product
+router.post('/edit/:id', productController.editPrice);
+
+//Route for deleting a product
+router.post('/delete/:id', productController.deleteProduct);
+
+//Route for getting all products
+router.get('/getById/:id', productController.getProductById);
+
+// Route for getting product details
+router.get('/getByName/:name', productController.getProductByName);
+
+//Route for getting all the products
+router.get('/getAll', productController.getAllProducts);
+// Add more routes as needed
+
+module.exports = router;
